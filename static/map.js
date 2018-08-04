6ebbe5d211db61573959ee2d6caa9b8aeed83775
Scallen <s1788817@ed.ac.uk>
2018-08-04 17:33:07 +1000

    External js and places testing

--- static/map.js
+++ static/map.js
@@ -1,16 +1,50 @@
-var map;
 function initMap() {
 
-  var home = {lat: -33.881904, lng: 151.195156};
+  var directionsService = new google.maps.DirectionsService();
+  var directionsDisplay = new google.maps.DirectionsRenderer();
+  directionsDisplay.setMap(map);
 
-  map = new google.maps.Map(document.getElementById('map'), {
+  var home = {lat:-33.8818423,lng: 151.1946505};
+  var homeLL = new google.maps.LatLng(home.lat,home.lng);
+
+  var map = new google.maps.Map(document.getElementById('map'), {
     center: home,
-    zoom: 18
+    zoom: 15
   });
 
-  var marker = new google.maps.Marker({
-    position: home,
-    map: map,
-    title: "Here we are",
-  })
+  var request = {
+    location: homeLL,
+    radius: '100',
+    type: []
+  };
+
+  service = new google.maps.places.PlacesService(map);
+
+  var randPlace = service.nearbySearch(request, callback);
+/*
+  var request = {
+    origin: homeLL,
+    destination: randPlace,
+    travelMode: 'WALKING'
+  };
+  directionsService.route(request, function(result, status) {
+    if (status == 'OK') {
+      directionsDisplay.setDirections(result);
+    }
+  });
+*/
+
+
+}
+
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      var place = results[i];
+      //createMarker(results[i]);
+      console.log(results[i])
+    }
+    return results[0];
+  }
 }
